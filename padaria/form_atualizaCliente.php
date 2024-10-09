7f2b665e0b26d81db4e97670fa9311958f1bf04d
Diego Pontes <ediego912@gmail.com>
2024-10-09 11:25:56 -0300

    formulario atualizado

--- padaria/form_atualizaCliente.php
+++ padaria/form_atualizaCliente.php
@@ -13,15 +13,55 @@
 
 ?>
 
-<form method="POST" action="atualizaCliente.php">
+<!DOCTYPE html>
+<html lang="en">
 
-    <input type="hidden" name="id" value="<?php echo $clienteSelecionado['id'];?>">
-    
-    Nome: <input type="text" name="nome" value="<?php echo $clienteSelecionado['nome'];?>">
-    Telefone: <input type="text" name="telefone" value="<?php echo $clienteSelecionado['telefone'];?>">
-    Email: <input type="email" name="email" value="<?php echo $clienteSelecionado['email'];?>">
-    Cpf: <input type="text" name="cpf" value="<?php echo $clienteSelecionado['cpf'];?>">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Atualizar Cliente</title>
+</head>
 
-    <input type="submit" value="Atualizar">
+<body>
+    <div class="container mt-5">
+        <h2 class="text-center"> Atualizar Cliente </h2>
 
-</form>
+        <form method="POST" action="atualizaCliente.php">
+            <input type="hidden" name="id" value="<?php echo $clienteSelecionado['id']; ?>">
+
+            <div class="mt-5 mb-3 row justify-content-center">
+                <div class="col-md-6">
+                    <label for="nome" class="form-label"> Nome </label>
+                    <input type="text" class="form-control" name="nome" value="<?php echo $clienteSelecionado['nome']; ?>">
+                </div>
+            </div>
+            <div class="mb-3 row justify-content-center">
+                <div class="col-md-6">
+                    <label for="email" class="form-label"> Email </label>
+                    <input type="email" class="form-control" name="email" value="<?php echo $clienteSelecionado['email']; ?>">
+                </div>
+            </div>
+            <div class="mb-3 row justify-content-center">
+                <div class="col-md-3">
+                    <label for="telefone" class="form-label"> Telefone </label>
+                    <input type="text" class="form-control" name="telefone" value="<?php echo $clienteSelecionado['telefone']; ?> ">
+                </div>
+                <div class="col-md-3">
+                    <label for="cpf" class="form-label"> CPF </label>
+                    <input type="text" class="form-control" name="cpf" value="<?php echo $clienteSelecionado['cpf']; ?>">
+                </div>
+            </div>
+
+            <div class="text-center mt-5">
+                <button type="submit" class="btn btn-primary"> Atualizar </button>
+            </div>
+        </form>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
